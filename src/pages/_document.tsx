e501721dbe89dde954dd00822271c4e5151ac889
fsantos <flavio.santos@allcomsistemas.com.br>
2022-03-28 09:04:48 -0300

    :wrench: Melhorias de SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,15 +13,41 @@ export default class MyDocument extends Document {
 
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#f59e0b" />
-          <meta name="keywords" content="Flavio Santos" />
-          <meta name="title" content="Flavio Santos" />
-          <meta name="description" content="Portfolio Flávio Santos | Dev" />
+          <meta
+            name="keywords"
+            content="Flavio Santos, porfolio, Portfolio Flavio Santos, flavio santos dev, developer, web developer, react, nextjs, CSS3"
+          />
+          <meta name="title" content="Flávio Santos Dev" />
+          <meta
+            name="description"
+            content="Contrate um desenvolvedor web profissional. Criando layouts modernos, soluções interativas e manutenção de sites."
+          />
 
-          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content="Portfolio Flávio Santos" />
           <meta property="og:locale" content="pt_BR" />
-          <meta property="og:site_name" content="Flavio Santos" />
-          <meta property="og:title" content="Flavio Santos" />
-          <meta property="og:description" content="Flavio Santos Portfolio" />
+          <meta property="og:url" content="https://www.flaviosantos.dev/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Flávio Santos Dev" />
+          <meta
+            property="og:description"
+            content="Contrate um desenvolvedor web profissional. Criando layouts modernos, soluções interativas e manutenção de sites."
+          />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="flaviosantos.dev" />
+          <meta
+            property="twitter:url"
+            content="https://www.flaviosantos.dev/"
+          />
+          <meta name="twitter:title" content="Flávio Santos Dev" />
+          <meta
+            name="twitter:description"
+            content="Contrate um desenvolvedor web profissional. Criando layouts modernos, soluções interativas e manutenção de sites."
+          />
+          <meta
+            name="twitter:image"
+            content="https://flaviosantos.dev/_next/image?url=%2Fphoto.jpg&w=384&q=75"
+          />
         </Head>
         <body>
           <Main />
